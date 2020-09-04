f15166426df8fd2e6f61ec693def63a39bb1338c
nday-ldgz <70750698+nday-ldgz@users.noreply.github.com>
2020-09-04 17:39:59 +0800

    Add files via upload
    配置文件

---
+++ lib/config.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python3
+
+'''
+2020 Greekn developers (Test-Create-tools@v1)
+
+'''
+
+import sys
+
+'''
+配置文件 
+'''
+
+def vultr_config(): #vultr KEY 
+    api_key = "5MAYVYwN6UGG2N6ZZBZD7ZTHQFZOSKzLLX4Q" #替换成自己的key
+    return api_key
+
+    
+    
+
